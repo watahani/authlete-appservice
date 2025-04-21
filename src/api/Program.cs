8ed046e09b882bcba3364d1d26aa3062ea0c73be
Wataru Haniyama <wataru.haniyama@authlete.com>
2025-04-21 07:11:47 +0000

    comment out the code for debugging

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using api.Tools;
 using ModelContextProtocol.Server;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole(consoleLogOptions =>
@@ -19,7 +20,16 @@ builder.Services
 
 var app = builder.Build();
 
-// app.MapGet("/", () => "Hello from API!");
+// app.MapGet("/env", [Authorize]() =>
+// {
+//     var env = Environment.GetEnvironmentVariables();
+//     var sb = new StringBuilder();
+//     foreach (DictionaryEntry entry in env)
+//     {
+//         sb.AppendLine($"{entry.Key}: {entry.Value}");
+//     }
+//     return Results.Text(sb.ToString(), MediaTypeNames.Text.Plain);
+// });
 
 // app.MapGet("/headers", [Authorize](HttpRequest req) =>
 // {
